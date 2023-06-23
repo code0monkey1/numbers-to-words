37e8e1b0dc052cf65fe01254b55627d8f7783488
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 06:52:00 +0000

    23 Jun 2023, 12:22

--- test/numbers-to-words.test.ts
+++ test/numbers-to-words.test.ts
@@ -32,88 +32,92 @@ describe('numbers-to-words', () => {
 
 
      describe('double numbers',()=>{
-
-         test.each([
-          {num:10,word:'ten'},
-          {num:11,word:'eleven'},
-          {num:12,word:'twelve'},
-          {num:13,word:'thirteen'},
-          {num:14,word:'fourteen'},
-          {num:15,word:'fifteen'},
-          {num:16,word:'sixteen'},
-          {num:17,word:'seventeen'},
-          {num:18,word:'eighteen'},
-          {num:19,word:'nineteen'},
-        ])('$num gives $word',({num,word})=>{
-           
-           //Arrange
-             const sut= getNumberToWords()
-
-            //Act
-            const result= sut.convert(num)
+       
+        describe('between ten and twenty', () => {
+          test.each([
+           {num:10,word:'ten'},
+           {num:11,word:'eleven'},
+           {num:12,word:'twelve'},
+           {num:13,word:'thirteen'},
+           {num:14,word:'fourteen'},
+           {num:15,word:'fifteen'},
+           {num:16,word:'sixteen'},
+           {num:17,word:'seventeen'},
+           {num:18,word:'eighteen'},
+           {num:19,word:'nineteen'},
+         ])('$num gives $word',({num,word})=>{
             
-            //Assert
-            expect(result).toBe(word)
+            //Arrange
+              const sut= getNumberToWords()
+  
+             //Act
+             const result= sut.convert(num)
+             
+             //Assert
+             expect(result).toBe(word)
+  
+          })
+          
+        })
+        
 
-         })
+        describe('multiple of tens',()=>{
+   
+            test.each([
+             {num:10,word:'ten'},
+             {num:20,word:'twenty'},
+             {num:30,word:'thirty'},
+             {num:40,word:'forty'},
+             {num:50,word:'fifty'},
+             {num:60,word:'sixty'},
+             {num:70,word:'seventy'},
+             {num:80,word:'eighty'},
+             {num:90,word:'ninety'},
+           ])('$num gives $word',({num,word})=>{
+              
+              //Arrange
+                const sut= getNumberToWords()
+   
+               //Act
+               const result= sut.convert(num)
+               
+               //Assert
+               expect(result).toBe(word)
+   
+            })
+   
+   
+        })
 
-
+        describe('greater than 20 , plus ones digit',()=>{
+   
+       test.each([
+        {num:21,word:'twenty-one'},
+        {num:22,word:'twenty-two'},
+        {num:23,word:'twenty-three'},
+        {num:95,word:'ninety-five'},
+        {num:72,word:'seventy-two'},
+        {num:89,word:'eighty-nine'},
+        {num:99,word:'ninety-nine'},
+   
+      ])('$num gives $word',({num,word})=>{
+         
+         //Arrange
+           const sut= getNumberToWords()
+   
+          //Act
+          const result= sut.convert(num)
+          
+          //Assert
+          expect(result).toBe(word)
+   
+       })
+   
+   
+   })
      })
 
 
-     describe('double numbers multiple of tens',()=>{
 
-         test.each([
-          {num:10,word:'ten'},
-          {num:20,word:'twenty'},
-          {num:30,word:'thirty'},
-          {num:40,word:'forty'},
-          {num:50,word:'fifty'},
-          {num:60,word:'sixty'},
-          {num:70,word:'seventy'},
-          {num:80,word:'eighty'},
-          {num:90,word:'ninety'},
-        ])('$num gives $word',({num,word})=>{
-           
-           //Arrange
-             const sut= getNumberToWords()
-
-            //Act
-            const result= sut.convert(num)
-            
-            //Assert
-            expect(result).toBe(word)
-
-         })
-
-
-     })
-
-          describe('double numbers greater than 20 , plus ones digit',()=>{
-
-         test.each([
-          {num:21,word:'twenty-one'},
-          {num:22,word:'twenty-two'},
-          {num:23,word:'twenty-three'},
-          {num:95,word:'ninety-five'},
-          {num:72,word:'seventy-two'},
-          {num:89,word:'eighty-nine'},
-          {num:99,word:'ninety-nine'},
-
-        ])('$num gives $word',({num,word})=>{
-           
-           //Arrange
-             const sut= getNumberToWords()
-
-            //Act
-            const result= sut.convert(num)
-            
-            //Assert
-            expect(result).toBe(word)
-
-         })
-
-
-     })
 })
 
