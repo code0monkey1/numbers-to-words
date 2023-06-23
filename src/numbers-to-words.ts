30487cc8adbeaad147523ee133fa1b3e7ede15f7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 05:46:07 +0000

    23 Jun 2023, 11:16

--- src/numbers-to-words.ts
+++ src/numbers-to-words.ts
@@ -8,11 +8,24 @@ interface INumberToWord{
 
 export function getNumberToWords() :INumberToWord{
 
+  const NumberToWord:Record<number,string> ={
+      1:'one',
+      2:'two',
+      3:'three',
+      4: 'four',
+      5 :'five',
+      6 :'six',
+      7: 'seven',
+      8: 'eight',
+      9 : 'nine',
+      0: 'zero'
+  }
+
       return {
          convert(num:number){
-             if(num===1)
-               return 'one' 
-             else return 'two'
+
+             return NumberToWord[num]
+             
          }
       }
 }
