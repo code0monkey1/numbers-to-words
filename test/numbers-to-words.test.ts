14ecf440f20b0aadfa60a68d14a9c4c8490e0c48
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 08:51:54 +0000

    23 Jun 2023, 14:21

--- test/numbers-to-words.test.ts
+++ test/numbers-to-words.test.ts
@@ -153,11 +153,9 @@ describe('numbers-to-words', () => {
               test.each([
                 {num:1000,word:'one thousand'},
                 {num:2000,word:'two thousand'},
-                // {num:209,word:'two hundred nine'},
-                // {num:999,word:'nine hundred ninety-nine'},
-                // {num:872,word:'eight hundred seventy-two'},
-                // {num :583, word :'five hundred eighty-three'},
-                // {num :123, word :'one hundred twenty-three'}
+                {num:5000,word:'five thousand'},
+                {num:9000,word:'nine thousand'},
+  
               ])('$num gives $word',({num,word})=>{
 
                  //Arrange
