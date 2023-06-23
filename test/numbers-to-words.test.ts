708040f651d498fa09e54a0d3cc820c12d80ca9d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 05:50:25 +0000

    23 Jun 2023, 11:20

--- test/numbers-to-words.test.ts
+++ test/numbers-to-words.test.ts
@@ -5,7 +5,6 @@ describe('numbers-to-words', () => {
 
      describe('single number',()=>{
 
-         
          test.each([
           {num:0,word:'zero'},
           {num:1,word:'one'},
@@ -28,6 +27,36 @@ describe('numbers-to-words', () => {
             expect(result).toBe('one')
 
          })
+
+     })
+
+
+     describe('double number',()=>{
+
+          
+         test.each([
+          {num:10,word:'zero'},
+          // {num:1,word:'one'},
+          // {num:2,word:'two'},
+          // {num:3,word:'three'},
+          // {num:4,word:'four'},
+          // {num:5,word:'five'},
+          // {num:6,word:'six'},
+          // {num:7,word:'seven'},
+          // {num:8,word:'eight'},
+          // {num:9,word:'nine'},
+        ])('$num gives $word',()=>{
+           
+           //arrange
+             const sut= getNumberToWords()
+            //act
+            const result= sut.convert(10)
+            
+            //assert
+            expect(result).toBe('ten')
+
+         })
+
 
      })
 })
