66dbe29d6daea84c64b36f95df3580e19f3f7731
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 07:31:37 +0000

    23 Jun 2023, 13:01

--- src/numbers-to-words.ts
+++ src/numbers-to-words.ts
@@ -24,9 +24,9 @@ export function getNumberToWords() :INumberToWord{
                   const ones = getOnesWord(num%10)
                   
                   word = hundreds+
-                        ' hundred '+
-                        (tens!=='zero'?`${tens} `:'')+
-                        (ones!=='zero'?ones:'')
+                        ' hundred'+
+                        (tens!=='zero'?` ${tens}`:'')+
+                        (ones!=='zero'?` ${ones}`:'')
 
              }
              else if(isBetween21and99(num)){
