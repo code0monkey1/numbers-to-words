39f36e9282b810f10888e6f1d4bc1b28a19431e7
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 05:56:46 +0000

    23 Jun 2023, 11:26

--- test/numbers-to-words.test.ts
+++ test/numbers-to-words.test.ts
@@ -38,14 +38,14 @@ describe('numbers-to-words', () => {
          test.each([
           {num:10,word:'ten'},
           {num:11,word:'eleven'},
-          // {num:2,word:'two'},
-          // {num:3,word:'three'},
-          // {num:4,word:'four'},
-          // {num:5,word:'five'},
-          // {num:6,word:'six'},
-          // {num:7,word:'seven'},
-          // {num:8,word:'eight'},
-          // {num:9,word:'nine'},
+          {num:12,word:'twelve'},
+          {num:13,word:'thirteen'},
+          {num:14,word:'fourteen'},
+          {num:15,word:'fifteen'},
+          {num:16,word:'sixteen'},
+          {num:17,word:'seventeen'},
+          {num:18,word:'eighteen'},
+          {num:19,word:'nineteen'},
         ])('$num gives $word',({num,word})=>{
            
            //Arrange
