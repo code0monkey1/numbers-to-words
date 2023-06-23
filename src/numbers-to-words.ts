3ef41242d407de38567f13e0d5f62f9202fe0e97
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-06-23 11:17:34 +0000

    23 Jun 2023, 16:47

--- src/numbers-to-words.ts
+++ src/numbers-to-words.ts
@@ -1,4 +1,37 @@
+const NumberToWord:Record<number,string> ={
 
+      1:'one',
+      2:'two',
+      3:'three',
+      4: 'four',
+      5 :'five',
+      6 :'six',
+      7: 'seven',
+      8: 'eight',
+      9 : 'nine',
+      0: 'zero',
+
+      10:'ten',
+      11:'eleven',
+      12:'twelve',
+      13:'thirteen',
+      14:'fourteen',
+      15:'fifteen',
+      16:'sixteen',
+      17:'seventeen',
+      18:'eighteen',
+      19:'nineteen',
+
+      20:'twenty',
+      30:'thirty',
+      40:'forty',
+      50:'fifty',
+      60:'sixty',
+      70 :'seventy',
+      80 :'eighty',
+      90 :'ninety'
+
+  }
 interface INumberToWord{
 
       convert:(num : number)=>string
@@ -39,10 +72,22 @@ export function getNumberToWords() :INumberToWord{
 
     
     if(isPureMultipleOfHundred(num)){
+              
+           let word;
              
             const firstTwo= num/100
-    
-            return getTens(firstTwo)+' hundred'
+            
+            if(isBetween21and99(firstTwo)){
+                 word= getTens(firstTwo)
+            }
+            else{
+                 word=NumberToWord[firstTwo]
+            }
+            
+            word+=+' hundred'
+
+
+            return word
       }
  
     const thousands = getThousandsWord(num)
@@ -154,37 +199,3 @@ function getThousandsWord(num :number):string{
    return NumberToWord[thousands]+ ' thousand'
 }
 
-const NumberToWord:Record<number,string> ={
-
-      1:'one',
-      2:'two',
-      3:'three',
-      4: 'four',
-      5 :'five',
-      6 :'six',
-      7: 'seven',
-      8: 'eight',
-      9 : 'nine',
-      0: 'zero',
-
-      10:'ten',
-      11:'eleven',
-      12:'twelve',
-      13:'thirteen',
-      14:'fourteen',
-      15:'fifteen',
-      16:'sixteen',
-      17:'seventeen',
-      18:'eighteen',
-      19:'nineteen',
-
-      20:'twenty',
-      30:'thirty',
-      40:'forty',
-      50:'fifty',
-      60:'sixty',
-      70 :'seventy',
-      80 :'eighty',
-      90 :'ninety'
-
-  }
